8eb4545831be951cf72bdb0bd35099e6e48a5e7f
Travis Glasrud <travis.glasrud@libertymutual.com>
2020-09-26 17:53:10 -0500

    updating the node server config

--- gcp/config/server.js
+++ gcp/config/server.js
@@ -1,7 +1,17 @@
 var express = require('express');
 var app = express();
 var port = 3000;
+var web_path = '/var/www/ighestates';
 
-app.use(express.static('/var/www/ighestates'));
+app.use(express.static(web_path));
+app.set('views', app.set('port', web_path+'/views'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
 
-app.listen(port);
+app.get('*', function (req, res) {
+  res.render(web_path + '/index.html');
+});
+
+app.listen( port, function() {
+  console.log( 'Express server listening on port %d kinda fine like!', port );
+});
